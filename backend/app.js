5600ef37cdb4cd7f4467bafc205661c9a5178728
Vishwassaurabh <techvishwasindia@gmail.com>
2026-05-21 17:26:48 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ mongoose
 
 //! Cors config
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://saurabh-expense-tracker-website.netlify.app"],
 };
 app.use(cors(corsOptions));
 
